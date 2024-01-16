bb696f053f62f0da1c93d0c2a173124b24daeb28
MahdiMoosavi <3535bahr2089@gmail.com>
2024-01-16 14:55:43 +0330

    add emojies file

--- Reversi Othello.cpp
+++ Reversi Othello.cpp
@@ -3,16 +3,7 @@ using namespace std;
 
 
 
-/* void FindCodePlace(int long array[][]){
-		for(int i = 0; i<8; i++)
-		{
-			for(int j = 0; j<8; j++)
-			{				
-				array[i][j] = 1 ;
-			}
-		}
-}
- */
+
 int main ()
 {
 	int sum = 0;
@@ -74,6 +65,62 @@ int main ()
 
 		cout << endl;
 
-		 /* \U0001F534 */
+		/* \U0001F534 */
 	}
+
+
+		int TargetBlue;
+		string TargetConditions;
+
+		while (true){
+
+			cin >> TargetBlue ;
+
+			for(int i = 0; i<8; i++)
+			{
+				for(int j = 0; j<8; j++)
+				{
+					if(faza [i][j]>= 10)
+					{
+						if (faza [i][j] == 28 || faza [i][j] == 37 )
+						{
+							cout <<  " " <<  " \U0001F534 " << " " ;
+						}
+
+						else if(faza [i][j] == 29 || faza [i][j] == 36)
+						{
+							cout <<  " " << " \U0001F535 "  << " " ;
+						}
+
+						else if (faza [i][j] % 8 == 0)
+						{
+							cout << " " << " " << faza [i][j] << " " << " " << endl;
+						}
+						
+						else{
+							cout << " " << " " << faza [i][j] << " " << " ";	
+						}
+						
+					}
+
+					else if (faza [i][j]<10)
+					{	
+
+						if (faza [i][j] == 8)
+						{
+							cout << " " << " 0" << faza [i][j] << " " << " " << endl;
+						}
+						else{
+							cout << " " <<  " 0" << faza [i][j] << " " <<  " ";
+						}
+						
+					}
+				}
+
+				cout << endl;
+
+				/* \U0001F534 */
+			}
+			
+		}
 }
